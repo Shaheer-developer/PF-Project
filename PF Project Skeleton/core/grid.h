6c5d89a2ee9c61b4800a66cd8e64021593cf0603
SudaisMahmood <i252524@isb.nu.edu.pk>
2025-11-29 17:39:11 +0500

    Add grid.h header with function declarations

--- PF Project Skeleton/core/grid.h
+++ PF Project Skeleton/core/grid.h
@@ -8,25 +8,25 @@
 // ============================================================================
 
 // Check if a position is within grid bounds
-bool isInBounds();
+bool isInBounds(int x, int y);
 
 // Check if a tile is a track (can trains move on it?)
-bool isTrackTile();
+bool isTrackTile(char t);
 
 // Check if a tile is a switch (A-Z)
-bool isSwitchTile();
+bool isSwitchTile(char t);
 
 // Get the switch index (0-25) from a switch character (A-Z)
-int getSwitchIndex();
+int getSwitchIndex(char t);
 
 // Check if a position is a spawn point
-bool isSpawnPoint();
+bool isSpawnPoint(int x, int y);
 
 // Check if a position is a destination point
-bool isDestinationPoint();
+bool isDestinationPoint(int x, int y);
 
 // Place or remove a safety tile at a position (for mouse editing)
 // Returns true if successful
-bool toggleSafetyTile();
+bool toggleSafetyTile(int x, int y);
 
 #endif

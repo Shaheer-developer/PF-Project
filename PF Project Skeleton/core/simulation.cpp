2e9322f3dc7115891afc6eb8cf8c2d05bc10e223
Shaheer-developer <m.shaheer062006@gmail.com>
2025-11-30 16:23:43 +0500

    added to terminal output

--- PF Project Skeleton/core/simulation.cpp
+++ PF Project Skeleton/core/simulation.cpp
@@ -3,8 +3,10 @@
 #include "trains.h"
 #include "switches.h"
 #include "io.h"
+#include <iostream>
 #include <cstdlib>
 #include <ctime>
+using namespace std;
 
 // ============================================================================
 // SIMULATION.CPP - Implementation of main simulation logic
@@ -13,38 +15,70 @@
 // ----------------------------------------------------------------------------
 // SIMULATE ONE TICK
 // ----------------------------------------------------------------------------
+void printGridToTerminal()
+{
+    cout << "\n========== Tick: " << tick << " ==========\n";
 
-void simulateOneTick() {
-        //spawn
+    for (int r = 0; r < rows; r++)
+    {
+        for (int c = 0; c < cols; c++)
+        {
+            // checking if train is present, then we wil show train and if train not present then show the tile
+            bool trainHere = false;
+            for (int i = 0; i < numTrains; i++)
+            {
+                if (trainState[i] == TRAIN_STATE_ACTIVE &&
+                    train_x[i] == c && train_y[i] == r)
+                {
+                    cout << i; // show train
+                    trainHere = true;
+                    break;
+                }
+            }
+            if (!trainHere)
+            {
+                cout << grid[r][c]; // show tile
+            }
+        }
+        cout << endl;
+    }
+    cout << "=====================================\n";
+}
+
+void simulateOneTick()
+{
+    // spawn
     spawnTrainsForTick();
 
-    //route determination
+    // route determination
     determineAllRoutes();
 
-    //update switch counter
+    // update switch counter
     updateSwitchCounters();
 
-    //flip acc to k values
+    // flip acc to k values
     queueSwitchFlips();
 
-    //movement
+    // movement
     moveAllTrains();
 
-    //change state and reset counter
+    // change state and reset counter
     applyDeferredFlips();
-    
+
+    printGridToTerminal();
 }
 
 // ----------------------------------------------------------------------------
 // CHECK IF SIMULATION IS COMPLETE
 // ----------------------------------------------------------------------------
 
-bool isSimulationComplete() {
+bool isSimulationComplete()
+{
 }
 
-void updateTick(){
+void updateTick()
+{
     tick++;
 
     simulateOneTick();
-
 }
